2572f3fecb788d0c354f2d8e720b74b63031c4ab
Braddyers <braddyers@gmail.com>
2025-02-17 13:59:44 +0200

    Edited comments in numpy_task.py

--- All Tasks/Python Packages Task/numpy_task.py
+++ All Tasks/Python Packages Task/numpy_task.py
@@ -47,37 +47,37 @@ print(arr)
 # There are multiple ways to obtain the specified values:
 # a. 20.0
 # Using indexing
-answer = arr[1, 0, 3]             # Selects [second matrix, first row, fourth column]
+answer = arr[1, 0, 3]                                      # Selects [second matrix, first row, fourth column]
 print(f"a. {answer}")
 # Or flatten the array to 1D and then access with new index number (starting at index 0)
 answer = arr.flatten()[19]
 print(f"a. {answer}")
 
 # b. [ 9. 10. 11. 12.]
-answer = arr[0, 2]                # Selects [first matrix, third row]
+answer = arr[0, 2]                                         # Selects [first matrix, third row]
 print(f"\nb. {answer}")
 
 # c. [[33. 34. 35. 36.] [37. 38. 39. 40.] [41. 42. 43. 44.] [45. 46. 47. 48.]]
-answer = arr[2]                   # Selects [third matrix]
+answer = arr[2]                                            # Selects [third matrix]
 print(f"\nc. {answer}")
 
 # d. [[5. 6.], [21. 22.] [37. 38.]]
-answer = arr[:, 1, 0:2]           # Selects [all matrices, second row, first two columns]
+answer = arr[:, 1, 0:2]                                    # Selects [all matrices, second row, first two columns]
 print(f"\nd. {answer}")
 
 # e. [[36. 35.] [40. 39.] [44. 43.] [48. 47.]]
-answer = arr[2, :, -1:-3:-1]      # Selects [third matrix, all rows, last two columns in reverse]
+answer = arr[2, :, -1:-3:-1]                               # Selects [third matrix, all rows, last two columns in reverse]
 print(f"\ne. {answer}")
 
 # f. [[13. 9. 5. 1.] [29. 25. 21. 17.] [45. 41. 37. 33.]]
 answer = arr[:, :, 0][:, ::-1]
-# [:, :, 0] means [all matrices, all rows, first column]
-# [:, ::-1] means [all rows, (start at last column end, end at last column, move backwards by 1)] 
+# [:, :, 0] means select [all matrices, all rows, first column]
+# [:, ::-1] means select [all rows, (start at last column end, end at last column, move backwards by 1)] 
 print(f"\nf. {answer}")           
 
 # g. [[1. 4.] [45. 48.]]
-indices = [0, 3, 44, 47]  # Flattened indices of desired values
-answer = arr.flatten()[indices].reshape(2, 2)
+indices = [0, 3, 44, 47]                                   # Indices of desired values of flattened array
+answer = arr.flatten()[indices].reshape(2, 2)              # Flatten array and reshape it to include desired indices
 print(f"\ng. {answer}")
 
 # h. [[25. 26. 27. 28.], [29. 30. 31. 32.], [33. 34. 35. 36.], [37. 38. 39. 40.]]
